f9a1e7ec0694f632bd40d561391f463db62c219e
LucaFerrantedev <42522463+LucaFerrantedev@users.noreply.github.com>
2025-11-17 19:04:39 -0300

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Aclaracion:
 # composición de ingresos de los recursos = índice de desarrollo humano que combina el Producto Nacional Bruto (PNB) per cápita,
 # los años de escolaridad y la esperanza de vida al nacer, y sirve como un fuerte indicador del nivel de desarrollo socioeconómico de un país
 # (el índice varía de 0 a 1) 0 significando muy bajos ingresos y 1 ingresos muy altos.
@@ -14,11 +13,11 @@ df.columns = df.columns.str.strip()
 X_col = 'Income composition of resources'
 Y_col = 'Life expectancy'
 
-# elimina las filas con datos faltantes en las columnas X_col y Y_col
+# elimina las filas con datos que faltan
 df_clean = df[[X_col, Y_col]].dropna()
-
-X = np.array(df_clean[X_col]).reshape(-1, 1)
-Y = np.array(df_clean[Y_col]).reshape(-1, 1)
+X = np.array(df_clean[df_clean[X_col] >0.2][X_col]).reshape(-1, 1) # CORREGIDO EN CLASE: APLICAMOS FILTRO PARA IGNORAR LOS PAISES CON INDICE MENOR A 0.2
+Y = np.array(df_clean[df_clean[X_col] >0.2][Y_col]).reshape(-1, 1) # CORREGIDO EN CLASE: APLICAMOS FILTRO PARA IGNORAR LOS PAISES CON INDICE MENOR A 0.2
+                                                                    # ESTO ME AYUDO A QUE QUEDE MAS LIMPIO EL GRAFICO Y EL SCORE SUBA DE 0.502 A 0.760
 
 Y_log = np.log(Y)
 
@@ -28,11 +27,11 @@ model.fit(X, Y_log)
 
 r_squared = model.score(X, Y_log)
 
-# obtener coeficientes
+
 intercept = model.intercept_[0]
 coef = model.coef_[0][0]
 
-# rango de valores X para la curva de regresión
+# rango de valores X
 X_new = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
 
 # predecir Y
@@ -45,9 +44,9 @@ ax.plot(X_new, Y_new, color='red', label=r'Regresión Exponencial')
 
 print("El score es: ", r_squared)
 
-ax.set_xlabel(f'Índice Composición de ingresos de los recursos')
+ax.set_xlabel('Índice Composición de ingresos de los recursos')
 ax.set_ylabel('Expectativa de vida (Años)')
-ax.set_title(f'Prediccion de Expectativa de Vida basada en Indice Composición de ingresos de los recursos')
+ax.set_title('Prediccion de Expectativa de Vida basada en Indice Composición de ingresos de los recursos')
 ax.legend()
 ax.grid(True)
 plt.show()
